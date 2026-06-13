7fc29d5457625cc47b0a2696a350a5281571021c
Sona <sonaaz0615@gmail.com>
2026-06-13 15:13:46 +0530

    pull request

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,18 @@
 import { Link } from "react-router-dom";
 
-function Home(){
+function Home() {
+  return (
+    <div style={{ padding: "24px", fontFamily: "Arial, sans-serif" }}>
+      <h1>Welcome Home</h1>
+      <p>Start shopping and add items directly to your cart.</p>
 
-return(
-<div>
-
-<h1>Welcome Home</h1>
-
-<Link to="/login">
-Go to Login
-</Link>
-
-</div>
-)
-
+      <div style={{ display: "flex", gap: "12px", flexWrap: "wrap" }}>
+        <Link to="/products">Browse Products</Link>
+        <Link to="/cart">View Cart</Link>
+        <Link to="/login">Go to Login</Link>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
