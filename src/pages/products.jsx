c47df92fbcdf3dba8e2edf14c6b8c7e705950969
Sona <sonaaz0615@gmail.com>
2026-06-13 13:22:47 +0530

    vvv

---
+++ src/pages/products.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+function Products() {
+    const products = [
+  {
+    id: 1,
+    name: "Men's Casual Shirt",
+    price: 999,
+    category: "Men",
+    image: "/images/shirt1.jpg"
+  },
+  {
+    id: 2,
+    name: "Women's Kurti",
+    price: 1299,
+    category: "Women",
+    image: "/images/kurti1.jpg"
+  },
+  {
+    id: 3,
+    name: "Denim Jacket",
+    price: 1999,
+    category: "Men",
+    image: "/images/jacket1.jpg"
+  },
+  {
+    id: 4,
+    name: "Cotton Saree",
+    price: 1499,
+    category: "Women",
+    image: "/images/saree1.jpg"
+  }
+];
+}
+
+export default products;
